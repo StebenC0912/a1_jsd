b7cf5a6c67c49a565f05f5f9dbb52a92c676cd7b
StebenC0912 <stebenc09122002@gmail.com>
2023-12-02 23:50:57 +0700

    a2 jsd

--- Database/DatabaseConnect.java
+++ Database/DatabaseConnect.java
@@ -3,22 +3,183 @@ package Database;
 import a1_2001040121.Patron;
 
 import java.sql.*;
-
 public class DatabaseConnect {
-    private static Connection connection = null;
-    private static Statement statement = null;
-    public static Connection connect() {
+
+    // Connect to the sqlite database with the file library.db
+    Connection connection = null;
+    private Statement stmt = null;
+
+    public Connection connect() {
         try {
             connection = java.sql.DriverManager.getConnection("jdbc:sqlite:library.db");
-            statement = connection.createStatement();
-            System.out.println("Connection to SQLite has been established.");
+            stmt = connection.createStatement();
+            System.out.println("Connected to database!");
         } catch (Exception e) {
             e.printStackTrace();
         }
         return connection;
     }
-    public void addPatron(Patron patron) throws SQLException {
-        String query = "INSERT INTO PATRON (PATRON_ID, NAME, DOB, EMAIL, PHONE_NUM, PATRON_TYPE) VALUES (?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS))
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insertPatron(String name, String dob, String email, String phone, String patronType) {
+        try {
+            connection = connect();
+            stmt.execute("INSERT INTO patron (name, dob, email, phone, patronType) VALUES ('" + name + "', '" + dob + "', '" + email + "', '" + phone + "', '" + patronType + "')");
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+    }
+
+    public void executeQuery(String s) {
+        try {
+            connection = connect();
+            stmt.execute(s);
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeConnection();
+    }
+
+    public void insertBook(String isbn, String title, String author, String genre, String publicationYear, int numberOfCopiesAvailable) {
+        try {
+            connection = connect();
+            stmt.execute("INSERT INTO book (isbn, title, author, genre, pubYear, numCopiesAvailable) VALUES ('" + isbn + "', '" + title + "', '" + author + "', '" + genre + "', '" + publicationYear + "', '" + numberOfCopiesAvailable + "')");
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void checkoutBook(int bookId, int patronId, String checkoutDate, String dueDateText) {
+        try {
+            connection = connect();
+            stmt.execute("INSERT INTO 'transaction' (book_id, patron_id, checkoutDate, dueDate) VALUES ('" + bookId + "', '" + patronId + "', '" + checkoutDate + "', '" + dueDateText + "')");
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int getNumberOfCopiesAvailable(int bookId) {
+        int numberOfCopiesAvailable = 0;
+        try {
+            connection = connect();
+            Statement stmt = connection.createStatement();
+            String query = "SELECT numCopiesAvailable FROM book WHERE id = '" + bookId + "'";
+            ResultSet rs = stmt.executeQuery(query);
+
+            if (rs.next()) {
+                numberOfCopiesAvailable = rs.getInt("numCopiesAvailable");
+            }
+            rs.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return numberOfCopiesAvailable;
+    }
+
+    public void decrementNumberOfCopiesAvailable(int bookId) {
+        try {
+            connection = connect();
+            stmt.executeUpdate("UPDATE book SET numCopiesAvailable = numCopiesAvailable - 1 WHERE id = '" + bookId + "'");
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void returnBook(int patronId, int bookId, String returnDateText) {
+        try {
+            connection = connect();
+            deleteTransaction(patronId, bookId);
+            System.out.println(patronId + " " + bookId);
+            setNumberOfCopiesAvailable(bookId, getNumberOfCopiesAvailable(bookId) + 1);
+            stmt.close();
+//            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTransaction(int patronId, int bookId) {
+        try {
+            connection = connect();
+            stmt.execute("DELETE FROM 'transaction' WHERE patron_id = '" + patronId + "' AND book_id = '" + bookId + "'");
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setNumberOfCopiesAvailable(int bookId, int numberOfCopiesAvailable) {
+        try {
+            connection = connect();
+            stmt.execute("UPDATE book SET numCopiesAvailable = " + numberOfCopiesAvailable + " WHERE id = '" + bookId + "'");
+            stmt.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getPatronId(String patronName) {
+        int patronId = 0;
+        try {
+            connection = connect();
+            Statement stmt = connection.createStatement();
+            String query = "SELECT id FROM patron WHERE name = '" + patronName + "'";
+            ResultSet resultSet = stmt.executeQuery(query);
+
+            if (resultSet.next()) {
+                patronId = resultSet.getInt("id");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return patronId;
+    }
+
+    public int getBookId(String bookName) {
+        int bookId = 0;
+        try {
+            connection = connect();
+            Statement stmt = connection.createStatement();
+            String query = "SELECT id FROM book WHERE title = '" + bookName + "'";
+            ResultSet resultSet = stmt.executeQuery(query);
+
+            if (resultSet.next()) {
+                bookId = resultSet.getInt("id");
+            }
+
+            System.out.println(bookId);
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bookId;
     }
 }
--- a1_2001040121/LibraryManProg.java
+++ a1_2001040121/LibraryManProg.java
@@ -1,11 +1,4 @@
 package a1_2001040121;
-import common.Genre;
-import common.PatronType;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class LibraryManProg {
 
